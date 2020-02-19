4c47cc678efe5040cf5ae8524f6827ec758e52e1
casperbox <casperbox.tech@gmail.com>
2020-02-18 20:21:20 -0700

    first real commit, setup basic working converter

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import js2ts from 'json-to-ts';
+import axios from 'axios';
+import { Input, Icon, Button, Grid, GridRow, GridColumn, Segment } from 'semantic-ui-react';
 
 function App() {
+  const [endpoint, setEndpoint] = useState();
+  const [apiResult, setApiResult] = useState();
+
+  const onRequest = async () => {
+    if (endpoint) {
+      try {
+        const res = await axios.get(endpoint);
+        setApiResult(res);
+      } catch (e) {
+        setApiResult({ message: e.message });
+      }
+    } else {
+      alert('No endpoint provided')
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{}}>
+      <Grid className="App-Container p-5" centered>
+        <GridRow className="m-5" textAlign="center">
+          <GridColumn className="test-border" mobile={16} tablet={13} computer={12}>
+            <Input
+              inverted
+              fluid
+              size="large"
+              placeholder="https://"
+              onChange={e => setEndpoint(e.target.value)}
+              value={endpoint}
+            />
+          </GridColumn>
+          <GridColumn className="test-border" mobile={16} tablet={3} computer={4} >
+            <Button
+              basic
+              circular
+              inverted
+              size="large"
+              // icon={<Icon name='bolt' />}
+              onClick={onRequest}
+              content={'Send Request'}
+            />
+          </GridColumn>
+        </GridRow>
+
+        <GridRow className="m-5">
+          <GridColumn className="test-border p-3" mobile={16} tablet={8} computer={8}>
+            {apiResult
+              ? <pre style={{ color: 'whitesmoke' }}>{JSON.stringify(apiResult, null, 4)}</pre>
+              : 'No Results'}
+          </GridColumn>
+          <GridColumn className="test-border p-3" mobile={16} tablet={8} computer={8}>
+            {apiResult
+              ? js2ts(apiResult).map(typeInterface => (
+                <pre style={{ color: 'whitesmoke' }}>{typeInterface}</pre>
+              ))
+              : "No Interfaces"}
+          </GridColumn>
+        </GridRow>
+      </Grid>
     </div>
   );
 }
